45d69d40a5d3a0bdbc86674c90ec78f7d759453b
nakamura26002002921-a11y <nakamura26002002921@gmail.com>
2026-04-14 23:06:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 
+pip install huggingface_hub
+
+python -c "from huggingface_hub import hf_hub_download; hf_hub_download(repo_id='bartowski/gemma-2-2b-it-GGUF', filename='gemma-2-2b-it-Q4_K_M.gguf', local_dir='/home/models', local_dir_use_symlinks=False)"
